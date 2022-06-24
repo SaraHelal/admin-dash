eff0be63a63d51017a23ba166056e6af71518887
SaraHelal <s.helal91@gmail.com>
2022-06-25 00:25:24 +0100

    widgets completed

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,18 @@
+import './featured.scss'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+const Featured = () => {
+  return (
+    <div className='featured'>
+        <div className="top">
+            <h1 className="title">Total revenue</h1>
+            <MoreVertIcon className='icon' fontSize='small' />
+
+        </div>
+        <div className="bottom">
+
+        </div>
+    </div>
+  )
+}
+
+export default Featured
