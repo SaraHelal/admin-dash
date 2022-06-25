c7adcecfa0a069ccb20795051bfb7bf50ee9b6d7
SaraHelal <s.helal91@gmail.com>
2022-06-25 10:57:30 +0100

    FeaturedChart completed

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,5 +1,10 @@
 import './featured.scss'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
 const Featured = () => {
   return (
     <div className='featured'>
@@ -9,7 +14,35 @@ const Featured = () => {
 
         </div>
         <div className="bottom">
-
+          <div className="featuredChart">
+            <CircularProgressbar value="75" text="75%" strokeWidth="5" />
+          </div>
+          <p className="title">Total sales made today</p>
+          <p className="amount">$420</p>
+          <p className="desc">Previous transactions processing. Last payments may not be included.</p>
+          <div className="summary">
+            <div className="item">
+              <div className="itemTitle">Target</div>
+              <div className="itemResult positive">
+                <KeyboardArrowUpIcon fontSize='small' />
+                <span className="resultAmount">$12.04K</span>
+              </div>
+            </div>
+            <div className="item">
+              <div className="itemTitle">Last week</div>
+              <div className="itemResult negative">
+                <KeyboardArrowDownIcon fontSize='small' />
+                <span className="resultAmount">$12.04K</span>
+              </div>
+            </div>
+            <div className="item">
+              <div className="itemTitle">Last month</div>
+              <div className="itemResult positive">
+                <KeyboardArrowUpIcon fontSize='small' />
+                <span className="resultAmount">$12.04K</span>
+              </div>
+            </div>
+          </div>
         </div>
     </div>
   )
